9ad79ccac5103917929b5e1b717258ce3f52016b
ShadyAK <51845947+ShadyAK@users.noreply.github.com>
2020-10-19 11:39:36 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "WELCOME"
+    return "Hello Soumybeeeeerrrrrrrrrr"
 @app.route('/predict',methods=['POST','GET'])
 def predict():
     if request.method=="POST":
